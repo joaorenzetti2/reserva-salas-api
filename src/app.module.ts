aa06d785b38e4c80e55f8a913e6ea618a74d1769
João Renzetti <jaorenzetti@outlook.com>
2025-11-17 01:31:56 -0300

    inicio do projeto com configuração inicial e entidades room e reservation

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { RoomModule } from './room/room.module';
+import { ReservationModule } from './reservation/reservation.module';
+
+@Module({
+  imports: [RoomModule, ReservationModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
